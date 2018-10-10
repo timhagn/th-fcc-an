ca6835ee44dc004cbc1cc130d29e2518d1aa6d7a
timhagn <mail@timhagn.com>
2018-10-10 16:47:56 +0200

    add session & passport

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+// Add express-session & passport.
+const session = require('express-session');
+const passport = require('passport');
+
 
 const app = express();
 
@@ -10,6 +14,15 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Add session handling.
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(passport.initialize());
 
 // Add 'pug' as render plugin.
 app.set('view engine', 'pug');

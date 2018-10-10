dd0a68a109add4a23e06a22d1f421b0e0de77cf3
timhagn <mail@timhagn.com>
2018-10-10 16:37:58 +0200

    add pug dependency

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Add 'pug' as render plugin.
+app.set('view engine', 'pug');
+
 app.route('/')
   .get((req, res) => {
     res.sendFile(process.cwd() + '/views/index.html');
+    res.render(process.cwd() + '/views/pug/index.pug');
   });
 
 app.listen(process.env.PORT || 3000, () => {
